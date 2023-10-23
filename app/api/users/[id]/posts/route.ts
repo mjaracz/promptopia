1c45e66dd2fef247f755aa5e553f644a854c582d
Michal Jaracz <mjara@softserveinc.com>
2023-10-23 16:28:33 +0200

    profile page

---
+++ app/api/users/[id]/posts/route.ts
@@ -0,0 +1,13 @@
+import Prompt from "@models/prompt";
+import { connectToDb } from "@utils/database"
+
+export const GET = async () => {
+  try {
+    await connectToDb();
+    const promptsDataList = await Prompt.find({}).populate('creator');
+
+    return new Response(JSON.stringify(promptsDataList), { status: 200 });
+  } catch (err) {
+    return new Response(JSON.stringify(err), { status: 500 });
+  }
+}
